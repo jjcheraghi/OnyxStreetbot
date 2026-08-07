df6a0438bb2052a79f9e24212171c64827718ae7
jjcheraghi <jjcheraghi@gmail.com>
2026-08-07 08:06:41 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ TOKEN = "8926088350:AAElvXxA3gADwdLbEFxyZ3WIiyIi0qow74Q"
 ADMIN_ID = 8356358583
 
 CHANNEL = "@Onyx_Street"
-BOT_USERNAME = "YOUR_BOT_USERNAME"
+BOT_USERNAME = "@OnyxStreetbot"
 
 bot = telebot.TeleBot(TOKEN)
 
@@ -139,4 +139,178 @@ def admin(message):
         message.chat.id,
         "Admin Panel",
         reply_markup=kb
+    )
+@bot.callback_query_handler(
+    func=lambda c: c.data == "add"
+)
+def add_mod(c):
+
+    if not is_admin(c.from_user.id):
+        return
+
+    admin_mode[c.from_user.id] = {
+        "step": "name"
+    }
+
+    bot.send_message(
+        c.message.chat.id,
+        "Send mod name:"
+    )
+
+
+@bot.message_handler(
+    func=lambda m:
+    is_admin(m.from_user.id)
+    and m.from_user.id in admin_mode
+    and admin_mode[m.from_user.id]["step"] == "name"
+)
+def get_name(m):
+
+    admin_mode[m.from_user.id]["name"] = m.text
+
+    admin_mode[m.from_user.id]["step"] = "type"
+
+    kb = types.InlineKeyboardMarkup()
+
+    kb.add(
+        types.InlineKeyboardButton(
+            "File",
+            callback_data="file"
+        )
+    )
+
+    kb.add(
+        types.InlineKeyboardButton(
+            "Link",
+            callback_data="link"
+        )
+    )
+
+    bot.send_message(
+        m.chat.id,
+        "Select type:",
+        reply_markup=kb
+    )
+
+
+@bot.callback_query_handler(
+    func=lambda c:
+    c.data in ["file","link"]
+)
+def select_type(c):
+
+    if not is_admin(c.from_user.id):
+        return
+
+    admin_mode[c.from_user.id]["step"] = c.data
+
+    if c.data == "file":
+
+        bot.send_message(
+            c.message.chat.id,
+            "Send file:"
+        )
+
+    else:
+
+        bot.send_message(
+            c.message.chat.id,
+            "Send download link:"
+        )
+
+
+
+@bot.message_handler(
+    content_types=["document"]
+)
+def receive_file(m):
+
+    if not is_admin(m.from_user.id):
+        return
+
+    if m.from_user.id not in admin_mode:
+        return
+
+
+    data = admin_mode[m.from_user.id]
+
+
+    if data["step"] != "file":
+        return
+
+
+    mods = load_mods()
+
+    mod_id = len(mods) + 1
+
+
+    mods.append({
+
+        "id": mod_id,
+        "name": data["name"],
+        "type": "file",
+        "file_id": m.document.file_id
+
+    })
+
+
+    save_mods(mods)
+
+    del admin_mode[m.from_user.id]
+
+
+    link = (
+        f"https://t.me/{BOT_USERNAME}"
+        f"?start=mod_{mod_id}"
+    )
+
+
+    bot.send_message(
+        m.chat.id,
+        "Mod added.\n\n"
+        f"Mod Link:\n{link}"
+    )
+
+
+
+@bot.message_handler(
+    func=lambda m:
+    is_admin(m.from_user.id)
+    and m.from_user.id in admin_mode
+    and admin_mode[m.from_user.id]["step"] == "link"
+)
+def receive_link(m):
+
+    data = admin_mode[m.from_user.id]
+
+    mods = load_mods()
+
+    mod_id = len(mods) + 1
+
+
+    mods.append({
+
+        "id": mod_id,
+        "name": data["name"],
+        "type": "link",
+        "url": m.text
+
+    })
+
+
+    save_mods(mods)
+
+    del admin_mode[m.from_user.id]
+
+
+    link = (
+        f"https://t.me/{BOT_USERNAME}"
+        f"?start=mod_{mod_id}"
+    )
+
+
+    bot.send_message(
+        m.chat.id,
+        "Mod added.\n\n"
+        f"Mod Link:\n{link}"
     )
